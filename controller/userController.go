1c64a5fa163bac11d8a82d3dd413465cd42803fd
joscha <zippelman@web.de>
2015-03-07 11:43:44 +0100

    resolved #2: register function added

---
+++ controller/userController.go
@@ -0,0 +1,66 @@
+package controller
+
+import (
+	"net/http"
+
+	"time"
+
+	"github.com/zenazn/goji/web"
+	"github.com/zippelmann/gtt/models"
+)
+
+type UserController interface {
+	Register(c web.C, w http.ResponseWriter, r *http.Request)
+}
+
+type UserControllerImpl struct {
+	userDao models.UserDao
+	crypter Crypter
+}
+
+func NewUserController(userDao models.UserDao, crypter Crypter) UserController {
+	return &UserControllerImpl{userDao, crypter}
+}
+
+func (controller *UserControllerImpl) Register(c web.C, w http.ResponseWriter, r *http.Request) {
+	if controller.isRegisterRequestValid(r) == false {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	if saveErr := controller.createUser(r); saveErr != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (controller *UserControllerImpl) isRegisterRequestValid(request *http.Request) bool {
+	if registerRequest, decodeErr := parseRequest(request); decodeErr != nil {
+		return false
+	} else if registerRequest.validate() != nil {
+		return false
+	} else if controller.isUsernameExisting(registerRequest.Username) {
+		return false
+	}
+	return true
+}
+
+func (controller *UserControllerImpl) createUser(request *http.Request) error {
+	parsedRequest, _ := parseRequest(request)
+	workTime, _ := time.ParseDuration(parsedRequest.WorkTime)
+	hash, crypterErr := controller.crypter.generateHash([]byte(parsedRequest.Password))
+	if crypterErr != nil {
+		return crypterErr
+	}
+	user := models.NewMinimalUserWithPassword(parsedRequest.Username, workTime, hash)
+	saveErr := controller.userDao.SaveWithPassword(user)
+	if saveErr != nil {
+		return saveErr
+	}
+	return nil
+}
+
+func (controller *UserControllerImpl) isUsernameExisting(username string) bool {
+	_, err := controller.userDao.FindByName(username)
+	return err == nil
+}
---
+++ controller/userController_test.go
@@ -0,0 +1,160 @@
+package controller
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+
+	"errors"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/mock"
+	"github.com/zenazn/goji/web"
+	. "github.com/zippelmann/gtt/models"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var _ = Describe("UserController", func() {
+	var (
+		userDao          *UserDaoMock
+		crypter          *CrypterMock
+		userController   UserController
+		responseRecorder *httptest.ResponseRecorder
+		context          web.C
+	)
+
+	BeforeEach(func() {
+		crypter = new(CrypterMock)
+		userDao = new(UserDaoMock)
+		userController = NewUserController(userDao, crypter)
+		responseRecorder = httptest.NewRecorder()
+		context = web.C{}
+	})
+
+	It("should detect valid register request.", func() {
+		jsonRequest := `{"username":"peter", "password":"secret", "workTime":"2h"}`
+		httpRequest, _ := http.NewRequest("GET", "localhost", strings.NewReader(jsonRequest))
+		userDao.On("FindByName", mock.Anything).Return(User{}, errors.New("not existing"))
+		userDao.On("SaveWithPassword", mock.Anything).Return(nil)
+		crypter.On("generateHash", mock.Anything).Return([]byte("hashedPassword"), nil)
+
+		userController.Register(context, responseRecorder, httpRequest)
+
+		Expect(responseRecorder.Code).To(Equal(http.StatusCreated))
+	})
+
+	It("should detect invalid register request - user exists.", func() {
+		jsonRequest := `{"username":"peter", "password":"secret", "workTime":"2h"}`
+		httpRequest, _ := http.NewRequest("GET", "localhost", strings.NewReader(jsonRequest))
+		userDao.On("FindByName", mock.Anything).Return(User{}, nil)
+		userDao.On("SaveWithPassword", mock.Anything).Return(nil)
+		crypter.On("generateHash", mock.Anything).Return([]byte("hashedPassword"), nil)
+
+		userController.Register(context, responseRecorder, httpRequest)
+
+		Expect(responseRecorder.Code).To(Equal(http.StatusNotAcceptable))
+	})
+	It("should detect invalid register request - user too short.", func() {
+		jsonRequest := `{"username":"pet", "password":"secret", "workTime":"2h"}`
+		httpRequest, _ := http.NewRequest("GET", "localhost", strings.NewReader(jsonRequest))
+		userDao.On("FindByName", mock.Anything).Return(User{}, errors.New("not existing"))
+		userDao.On("SaveWithPassword", mock.Anything).Return(nil)
+		crypter.On("generateHash", mock.Anything).Return([]byte("hashedPassword"), nil)
+
+		userController.Register(context, responseRecorder, httpRequest)
+
+		Expect(responseRecorder.Code).To(Equal(http.StatusNotAcceptable))
+	})
+	It("should detect invalid register request - invalid json.", func() {
+		jsonRequest := `"username":"peter", "password":"secret", "workTime":"2h"}`
+		httpRequest, _ := http.NewRequest("GET", "localhost", strings.NewReader(jsonRequest))
+		userDao.On("FindByName", mock.Anything).Return(User{}, errors.New("not existing"))
+		userDao.On("SaveWithPassword", mock.Anything).Return(nil)
+		crypter.On("generateHash", mock.Anything).Return([]byte("hashedPassword"), nil)
+
+		userController.Register(context, responseRecorder, httpRequest)
+
+		Expect(responseRecorder.Code).To(Equal(http.StatusNotAcceptable))
+	})
+	It("should cancel registration due to a hashing error.", func() {
+		jsonRequest := `"{username":"peter", "password":"secret", "workTime":"2h"}`
+		httpRequest, _ := http.NewRequest("GET", "localhost", strings.NewReader(jsonRequest))
+		userDao.On("FindByName", mock.Anything).Return(User{}, errors.New("not existing"))
+		userDao.On("SaveWithPassword", mock.Anything).Return(nil)
+		crypter.On("generateHash", mock.Anything).Return([]byte("hashedPassword"), errors.New("error while hashing"))
+
+		userController.Register(context, responseRecorder, httpRequest)
+
+		Expect(responseRecorder.Code).To(Equal(http.StatusNotAcceptable))
+	})
+	It("should return an error when creating a user fails.", func() {
+		jsonRequest := `{"username":"peter", "password":"secret", "workTime":"2h"}`
+		httpRequest, _ := http.NewRequest("GET", "localhost", strings.NewReader(jsonRequest))
+		userDao.On("FindByName", mock.Anything).Return(User{}, errors.New("not existing"))
+		userDao.On("SaveWithPassword", mock.Anything).Return(errors.New("db connection failed"))
+		crypter.On("generateHash", mock.Anything).Return([]byte("hashedPassword"), nil)
+
+		userController.Register(context, responseRecorder, httpRequest)
+
+		Expect(responseRecorder.Code).To(Equal(http.StatusNotAcceptable))
+	})
+})
+
+type CrypterMock struct {
+	mock.Mock
+}
+
+func (mock CrypterMock) generateHash(password []byte) ([]byte, error) {
+	args := mock.Called(password)
+	return args.Get(0).([]byte), args.Error(1)
+}
+
+func (mock CrypterMock) isSamePassword(hash, password []byte) bool {
+	args := mock.Called(hash, password)
+	return args.Bool(0)
+}
+
+type UserDaoMock struct {
+	mock.Mock
+}
+
+func (mock UserDaoMock) Save(user User) error {
+	args := mock.Called(user)
+	return args.Error(0)
+}
+
+func (mock UserDaoMock) SaveWithPassword(user UserWithPassword) error {
+	args := mock.Called(user)
+	return args.Error(0)
+}
+
+func (mock UserDaoMock) FindByID(id bson.ObjectId) (User, error) {
+	args := mock.Called(id)
+	return args.Get(0).(User), args.Error(1)
+}
+
+func (mock UserDaoMock) FindByName(name string) (User, error) {
+	args := mock.Called(name)
+	return args.Get(0).(User), args.Error(1)
+}
+
+func (mock UserDaoMock) AddPassword(id bson.ObjectId, password string) error {
+	args := mock.Called(id, password)
+	return args.Error(0)
+}
+
+func (mock UserDaoMock) AddPasswordByUser(username string, password string) error {
+	args := mock.Called(username, password)
+	return args.Error(0)
+}
+
+func (mock UserDaoMock) GetPassword(id bson.ObjectId) (string, error) {
+	args := mock.Called(id)
+	return args.String(0), args.Error(1)
+}
+
+func (mock UserDaoMock) Update(user User) error {
+	args := mock.Called(user)
+	return args.Error(0)
+}
