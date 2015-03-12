de7fb89679b95763f00ed4e6d70b535cc9e4ce38
joscha <zippelman@web.de>
2015-03-12 12:39:45 +0100

    added interval controller scaffold

---
+++ controller/intervalController_test.go
@@ -0,0 +1,25 @@
+package controller
+
+import (
+	. "github.com/onsi/ginkgo"
+	//	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Configparser", func() {
+	var (
+	//		userController   UserController
+	//		responseRecorder *httptest.ResponseRecorder
+	//		context          web.C
+	)
+
+	BeforeEach(func() {
+		//		userController = NewUserController(userDao, crypter)
+		//		responseRecorder = httptest.NewRecorder()
+		//		context = web.C{}
+	})
+	It("should start a new interval.", func() {
+		//		httpRequest, _ := http.NewRequest("POST", "localhost", strings.NewReader(""))
+
+		//		Expect(responseRecorder.Code).To(Equal(http.StatusCreated))
+	})
+})
